0edbe1cbad460bf0f85c9cd9d9cd0ed434e9f1b6
Xiao Yu <jasonyux17@gmail.com>
2021-01-30 17:27:38 +0800

    Initialized ptree syscall in Kernel
    - added ptree in syscall_64.tbl
    - initialized the ptree.c file
    - added the header file

---
+++ include/linux/prinfo.h
@@ -0,0 +1,13 @@
+#ifndef __KERNEL_PRINFO__
+#define __KERNEL_PRINFO__
+
+struct prinfo {
+	pid_t parent_pid;       /* process id of parent */
+	pid_t pid;              /* process id */
+	long state;             /* current state of process */
+	uid_t uid;              /* user id of process owner */
+	char comm[16];          /* name of program executed */
+	int level;              /* level of this process in the subtree */
+};
+
+#endif
---
+++ kernel/ptree.c
@@ -0,0 +1,6 @@
+#include <linux/prinfo.h>
+
+int ptree(struct prinfo *buf, int *nr, int root_pid)
+{
+	return 0;
+}
