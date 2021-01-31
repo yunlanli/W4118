d1a5f3e60b579451446c8928959b906b27dd4fc5
Yunlan Li <chnsidney@gmail.com>
2021-01-31 20:03:04 +0800

    extract the code for populating prinfo for a process into a function

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -1,5 +1,6 @@
 #include <linux/syscalls.h>
 #include <linux/prinfo.h>
+#include <linux/types.h>
 /* for struct task_struct */
 #include <linux/sched.h>
 #include <linux/sched/task.h>
@@ -12,6 +13,9 @@
 #include <linux/string.h>
 /* for errno macro */
 #include <uapi/asm-generic/errno-base.h>
+/* lock */
+#include <linux/rwlock.h>
+#include <linux/kfifo.h>
 
 static struct task_struct *get_root(int root_pid)
 {
@@ -21,24 +25,54 @@ static struct task_struct *get_root(int root_pid)
 	return find_task_by_vpid(root_pid);
 }
 
-SYSCALL_DEFINE3(ptree, struct prinfo *, buf, int *, nr, int, root_pid)
+struct task_list {
+	struct task_struct *task;
+	struct list_head *list_node;
+};
+
+int fill_prinfo(struct task_struct *task,
+		struct task_struct *parent,
+		struct prinfo *buf,
+		struct prinfo *tmp)
 {
-	struct task_struct *root = get_root(root_pid),
-			   *parent = root->real_parent;
-
-	struct prinfo tmp;
-	/* tmp.pid = task_tgid_vnr(root); */
-
-	tmp.parent_pid = parent == NULL ? 0 : parent->pid;
-	tmp.pid = root->pid;
-	tmp.state = root->state;
-	/* tmp.uid = from_kuid_munged(root->real_cred->user_ns, root->real_cred->uid); */
-	strcpy(tmp.comm, root->comm);
-	tmp.level = 0;
+	tmp->parent_pid = parent == NULL ? 0 : parent->pid;
+	tmp->pid = task->pid;
+	tmp->state = task->state;
+	/* tmp.uid = from_kuid_munged(task->real_cred->user_ns, task->real_cred->uid); */
+	strcpy(tmp->comm, task->comm);
+	tmp->level = 0;
 
 	/* copy pid to the buf */
-	if (copy_to_user(buf, &tmp, sizeof(struct prinfo)))
+	if (copy_to_user(buf, tmp, sizeof(struct prinfo)))
 		return -EFAULT;
+
+	return 0;
+}
+
+SYSCALL_DEFINE3(ptree, typeof(struct prinfo *), buf, int *, nr, int, root_pid)
+{
+	/* initialize linked-list for traversing the task list */
+
+
+	/* 
+	 * initialize variables that will be used for storing task_struct info
+	 */
+	int count = 0, fill_res;
+
+	/* populate prinfo of root_pid */
+	struct task_struct *task = get_root(root_pid),
+			   *parent = task->real_parent;
+	struct prinfo tmp;
+
+	if ((fill_res = fill_prinfo(task, parent, buf, &tmp)))
+		return fill_res;
+	count++;
+	buf++;
+
+	/* traverse the process tree */
+	read_lock(&tasklist_lock);
+
+	read_unlock(&tasklist_lock);
 
 	return 69;
 }
