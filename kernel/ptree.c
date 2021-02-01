5ea3c5546501e4970d22e3d824541ef3b3805495
Yunlan Li <yl4387@columbia.edu>
2021-02-01 00:37:37 -0800

    this does not work

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -16,6 +16,8 @@
 /* lock */
 #include <linux/rwlock.h>
 #include <linux/kfifo.h>
+#include <linux/list.h>
+#include <linux/printk.h>
 
 static struct task_struct *get_root(int root_pid)
 {
@@ -25,9 +27,9 @@ static struct task_struct *get_root(int root_pid)
 	return find_task_by_vpid(root_pid);
 }
 
-struct task_list {
+struct task_node {
 	struct task_struct *task;
-	struct list_head *list_node;
+	struct list_head list_node;
 };
 
 int fill_prinfo(struct task_struct *task,
@@ -49,28 +51,94 @@ int fill_prinfo(struct task_struct *task,
 	return 0;
 }
 
+struct task_struct *task_add(
+		struct task_struct *task,
+		struct list_head *tail, 
+		int *seen,
+		int *nr,
+		int first_child)
+{
+	if (*seen < *nr) {
+		struct task_struct *tmp;
+
+		if (first_child)
+			tmp = list_entry(&(task->children), struct task_struct, children);
+		else
+			tmp = list_entry(&(task->sibling), struct task_struct, sibling);
+
+		LIST_HEAD(tmp_head);
+
+		struct task_node tmp_task;
+		tmp_task.task = tmp;
+		tmp_task.list_node = tmp_head;
+
+		list_add(&tmp_head, tail);
+
+		/* update tail */
+		*tail = *tail->next;
+		(*seen)++;
+
+		return tmp;
+	}
+
+	return NULL;
+}
+
 SYSCALL_DEFINE3(ptree, typeof(struct prinfo *), buf, int *, nr, int, root_pid)
 {
-	/* initialize linked-list for traversing the task list */
-
-
 	/* 
 	 * initialize variables that will be used for storing task_struct info
 	 */
-	int count = 0, fill_res;
-
-	/* populate prinfo of root_pid */
-	struct task_struct *task = get_root(root_pid),
-			   *parent = task->real_parent;
+	int seen = 0, count = 0, fill_res;
+	struct task_struct *task_struct_ptr, *parent;
 	struct prinfo tmp;
+	struct task_node *tmp_task;
 
-	if ((fill_res = fill_prinfo(task, parent, buf, &tmp)))
-		return fill_res;
-	count++;
-	buf++;
+	/* initialize linked-list for traversing the task list */
+	struct task_node root;
+
+	LIST_HEAD(head);
+	INIT_LIST_HEAD(&head);
+	root.list_node = head;
+	
+	root.task = get_root(root_pid);
+
+	seen++;
+
+	/* keep track of the head and tail of the list */
+	struct list_head tail;
+	tail = head;
 
 	/* traverse the process tree */
 	read_lock(&tasklist_lock);
+
+//	while (count < *nr && !list_empty(&head)) {
+//		/* retrieve container */
+//		tmp_task = list_entry(&head, struct task_node, list_node);
+//
+//		/* populate prinfo */
+//		task_struct_ptr = tmp_task->task;
+//		parent = task_struct_ptr->real_parent;
+
+//		if ((fill_res = fill_prinfo(task_struct_ptr, parent, buf, &tmp)))
+//			return fill_res;
+//		count++;
+//		buf++;
+//		printk("%d process done.\n", count);
+//
+//		break;
+//
+		/* add tmp_task's children to the list */
+//		task_struct_ptr = task_add(task_struct_ptr, &tail, &seen, nr, 1);
+
+		/* add first_child's siblings */
+//		while (task_struct_ptr != NULL && !list_empty(&task_struct_ptr->sibling))
+//			task_struct_ptr = task_add(task_struct_ptr, &tail, &seen, nr, 0); 
+
+		/* delete and update head */
+//		head = *head.next;
+//		list_del(head.prev);
+//	}
 
 	read_unlock(&tasklist_lock);
 
