89eda728b10eb6343426238f8f6ad57f715a9fe2
Yunlan Li <yl4387@columbia.edu>
2021-02-03 17:17:22 +0800

    ptree syscall works properly
    - add BFS logic to traverse the process tree

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -29,62 +29,71 @@ static struct task_struct *get_root(int root_pid)
 }
 
 
-static inline void fill_prinfo(struct task_struct *task, struct prinfo *tmp, int level)
+static inline void fill_prinfo(struct task_struct *task, struct prinfo *tmp)
 {
 	tmp->pid = task->pid;
 	tmp->parent_pid = task->real_parent == NULL ? 0 : task->real_parent->pid;
 	tmp->state = task->state;
 	/* tmp.uid = from_kuid_munged(task->real_cred->user_ns, task->real_cred->uid); */
 	strncpy(tmp->comm, task->comm, sizeof(tmp->comm));
-	tmp->level = level;
-
 }
 
 SYSCALL_DEFINE3(ptree, typeof(struct prinfo *), buf, int *, nr, int, root_pid)
 {
 	struct task_struct *task;
-	struct prinfo *buf_tmp, *buf_hd;
+	struct prinfo *buf_tmp;
 	struct list_head *list, *head;
-	int nr_cp, level, seen, stored;
+	int max, level, seen, visited;
 
-	if (copy_from_user(&nr_cp, nr, sizeof(nr_cp)))
+	if (copy_from_user(&max, nr, sizeof(max)))
 		return -EFAULT;
 
 	
-	seen = 0, stored = 0, level = 0;
-	buf_tmp = (struct prinfo *) kmalloc(sizeof(struct prinfo) * (nr_cp), GFP_KERNEL);
-	buf_hd = buf_tmp;
+	seen = 0, visited = 0, level = 0;
+	buf_tmp = (struct prinfo *) kmalloc(sizeof(struct prinfo) * (max), GFP_KERNEL);
 
-	task = get_root(root_pid);
-	head = &task->children;
-	
-	fill_prinfo(task, buf_hd++, level);
-	
+	buf_tmp->pid = root_pid;
+	buf_tmp->level = 0;
 	seen++;
-	stored++;
 
 	printk(KERN_DEBUG "start traversal...\n");
 
 	/* traverse the process tree */
 	read_lock(&tasklist_lock);
 
-	/* children */
-	list_for_each(list, head) {
-		task = list_entry(list, struct task_struct, sibling);
-		fill_prinfo(task, buf_hd++, level);
+	/* prinfo for each process gets populated in 2 steps:
+	 * 	1. pid, level are filled when it's first seen
+	 * 	2. the rest gets populated when it gets visited again
+	 */
+	while (seen < max && visited < seen) {
+		/* process next element on queue: retrieve children, fill prinfo */
+		task = get_root(buf_tmp[visited].pid);
+		head = &task->children;
+		fill_prinfo(task, &buf_tmp[visited++]);
+		level = buf_tmp->level;
 
-		seen++;
-		stored++;
+		printk(KERN_DEBUG "get_root while holding lock works\n");
+
+		/* children */
+		list_for_each(list, head) {
+			if (seen < max) {
+				task = list_entry(list, struct task_struct, sibling);
+				buf_tmp[seen].pid = task->pid;
+				buf_tmp[seen++].level = level+1;
+			} else 
+				break;
+		}
 	}
+
 	read_unlock(&tasklist_lock);
 
 	printk(KERN_DEBUG "Unlocked, traversal done.\n");
 	
-	if (copy_to_user(buf, buf_tmp, sizeof(struct prinfo) * stored)
-			|| copy_to_user(nr, &stored, sizeof(int)))
+	if (copy_to_user(buf, buf_tmp, sizeof(struct prinfo) * visited)
+			|| copy_to_user(nr, &visited, sizeof(int)))
 			return -EFAULT;
 
 	kfree(buf_tmp);
 
-	return stored;
+	return visited;
 }
