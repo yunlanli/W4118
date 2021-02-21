21e7706bb7e2f0f5735c57a0ecb4691ea651b4e8
jasonyux <jasonyux17@gmail.com>
2021-02-21 02:48:01 -0800

    Added more prompts

--- test.c
+++ test.c
@@ -23,6 +23,9 @@ int main()
 	int sys_ret, wstatus;
 	long counter;
 	struct pstrace buf[500];
+	int enable_pid;
+	int pid_to_trace;
+	
 	pid_t pid = fork();
 
 	if (pid < 0) {
@@ -31,38 +34,46 @@ int main()
 		/* child process */
 		sleep(3);
 		/* TASK_RUNNING -> TASK_INTERRUPTIBLE */
-		printf("enter a number: \n");
-		scanf("%d", &sys_ret);
+		//printf("enter a number: \n");
+		//scanf("%d", &sys_ret);
 		/* TASK_INTERRUPTIBLE -> TASK_RUNNING */
 	} else {
 		/* enable tracing child process pid */
-		sys_ret = syscall(436, pid);
+		printf("child pid: %d\n\n", pid);
+		
+		printf("Enter enable pid: ");
+		scanf("%d", &enable_pid);
+		printf("Enter pid to trace: ");
+		scanf("%d", &pid_to_trace);
+		printf("Enter counter: ");
+		scanf("%ld", &counter);
+#if 1
+		sys_ret = syscall(436, enable_pid);
 		if (sys_ret)
 			printf("pstrace_enable returned: %d\n", sys_ret);
 
 		sys_ret = waitpid(pid, &wstatus, 0);
 		print_error(sys_ret);
-
 		/* child pid has exited, disable tracing */
 		sys_ret = syscall(437, pid);
 		if (sys_ret)
 			printf("pstrace_disable returned: %d\n", sys_ret);
 
 		/* get all state change records for child process */
-		counter = 0;
-		sys_ret = syscall(438, pid, buf, &counter);
-		if (sys_ret)
-			printf("pstrace_get returned: %d\n", sys_ret);
-
+		sys_ret = syscall(438, pid_to_trace, buf, &counter);
+		printf("pstrace_get returned: %d\n", sys_ret);
+		
+		struct pstrace *rec = buf;
+		for (int i = 0; i<sys_ret; i++){
+			if (rec->pid == pid)
+				printf("%s, %d, %ld\n", rec->comm, rec->pid, rec->state);
+		}
+#endif
 		/* clear all state chagne records of pid */
-		sys_ret = syscall(439, pid);
+		sys_ret = syscall(439, -1);
 		if (sys_ret)
 			printf("pstrace_clear returned: %d\n", sys_ret);
 
-		struct pstrace *rec = buf;
-		for (int i = 0; i<500; i++)
-			if (rec->pid == pid)
-				printf("%s, %d, %ld", rec->comm, rec->pid, rec->state);
 	}
 
 	return 0;
