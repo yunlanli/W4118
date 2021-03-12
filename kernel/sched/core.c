de8cac63b180ab94968b35f8b759462103e2c7d2
Da Shi <65839413+SupernovaAAC@users.noreply.github.com>
2021-03-12 16:42:52 +0800

    Added init_wrr_rq() to core.c sched_init

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6655,6 +6655,7 @@ void __init sched_init(void)
 		init_cfs_rq(&rq->cfs);
 		init_rt_rq(&rq->rt);
 		init_dl_rq(&rq->dl);
+		init_wrr_rq(&rq->wrr);
 #ifdef CONFIG_FAIR_GROUP_SCHED
 		root_task_group.shares = ROOT_TASK_GROUP_LOAD;
 		INIT_LIST_HEAD(&rq->leaf_cfs_rq_list);
