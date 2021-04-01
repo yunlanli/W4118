00a2999b69becc95614ced35d24cce8ca3b277e9
Xiao Yu <jasonyux17@gmail.com>
2021-04-01 06:10:50 -0700

    haha

--- test/vm_inspector.c
+++ test/vm_inspector.c
@@ -109,7 +109,7 @@ int main(int argc, char **argv)
 	if (pgtbl_info.pgdir_shift == pgtbl_info.p4d_shift)
 		level = 4;
 
-	temp_addr = mmap(NULL, level*PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+	temp_addr = mmap(NULL, 6*PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
 	
 	if (temp_addr == MAP_FAILED)
 		return -1;
@@ -149,7 +149,7 @@ int main(int argc, char **argv)
 			continue;
 		
 		printf("[ info ] pgd_entry_addr=%p\n", 
-				(unsigned long *)(args.fake_pgd + get_index(virt, pgtbl_info.pgdir_shift)));
+				(unsigned long *)(args.fake_pgd) + get_index(virt, pgtbl_info.pgdir_shift));
 		printf("[ info ] p4d_entry=%lx\n", fake_p4d_entry);
 		
 		if (level == 5) {
@@ -159,21 +159,25 @@ int main(int argc, char **argv)
 		}
 		else
 			fake_pud_entry = fake_p4d_entry;
-		printf("[ info ] pud_entry=%lx\n", fake_pud_entry);
-			
 		
+		printf("[ info ] pud_table_addr=%lx\n", fake_pud_entry);
+		
+		printf("[ info ] pud_table_index=%lx\n", get_index(virt, pgtbl_info.pud_shift));
+		printf("[ info ] pud_entry_addr=%lx\n", 
+				(fake_pud_entry) + 8* get_index(virt, pgtbl_info.pud_shift));
+			
 		if ((fake_pmd_entry = 
-			*((unsigned long *)(args.fake_puds) + get_index(virt, pgtbl_info.pud_shift))) == 0)
+			*((unsigned long *)(fake_pud_entry) + get_index(virt, pgtbl_info.pud_shift))) == 0)
 			continue;
-		printf("[ info ] pmd_entry=%lx\n", fake_pmd_entry);
+		printf("[ info ] pmd_table_addr=%lx\n", fake_pmd_entry);
 		
 		if ((pte_entries = 
-			*((unsigned long *)(args.fake_pmds) + get_index(virt, pgtbl_info.pmd_shift))) == 0)
+			*((unsigned long *)(fake_pmd_entry) + get_index(virt, pgtbl_info.pmd_shift))) == 0)
 			continue;
 		printf("[ info ] pte_entries=%lx\n", pte_entries);
 		
 		if ((pte_entry = 
-			*((unsigned long *)(args.page_table_addr) + get_index(virt, pgtbl_info.page_shift))) == 0)
+			*((unsigned long *)(pte_entries) + get_index(virt, pgtbl_info.page_shift))) == 0)
 		{
 			if (verbose)
 				printf("0xdead00000000 0x00000000000 0 0 0 0\n");
