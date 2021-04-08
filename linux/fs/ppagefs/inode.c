46671241276f60ffe5ce8c38c39c6f2bed7229a2
Xiao Yu <jasonyux17@gmail.com>
2021-04-08 07:33:07 -0700

    adding new directory success
    - successfully created a new, working directory using inode and dentry

--- linux/fs/ppagefs/inode.c
+++ linux/fs/ppagefs/inode.c
@@ -5,15 +5,25 @@
 #include <linux/fs_context.h>
 #include <uapi/linux/stat.h>
 
+static const struct inode_operations ppagefs_dir_inode_operations = {
+	.lookup		= simple_lookup,
+	.link		= simple_link,
+	.unlink		= simple_unlink,
+	.rmdir		= simple_rmdir,
+	.rename		= simple_rename,
+};
+
 static int ppagefs_fill_super(struct super_block *sb, struct fs_context *fc)
 {
+	struct inode *inode;
+	struct dentry *dentry;
 	int err = 0;
 	static const struct tree_descr ppage_files[] = {
 		{NULL},
 		{NULL},
-		{"jason", &simple_dir_operations},
-		{"yunlan", &simple_dir_operations, S_IFDIR},
-		{"dashi", &simple_dir_operations},
+		{"jason", &simple_dir_operations, S_IRUSR | S_IWUSR},
+		{"yunlan", &simple_dir_operations, S_IRUSR | 01000},
+		{"dashi", &simple_dir_operations, 0755},
 		{""}
 	};
 
@@ -24,6 +34,25 @@ static int ppagefs_fill_super(struct super_block *sb, struct fs_context *fc)
 	err  =  simple_fill_super(sb, PPAGEFS_MAGIC, ppage_files);
 	if (err)
 		goto fail;
+
+	/* attempts to create a folder */
+	dentry = d_alloc_name(sb->s_root, "nieh");
+	if (!dentry)
+		goto fail;
+	inode = new_inode(sb);
+	if (!inode) {
+		dput(dentry);
+		goto fail;
+	}
+	inode->i_mode = S_IFDIR | 0755;
+	inode->i_atime = inode->i_mtime = inode->i_ctime = current_time(inode);
+	inode->i_op = &ppagefs_dir_inode_operations;
+	inode->i_fop = &simple_dir_operations; /* changed from i_fop */
+	inode->i_ino = 5;
+	/* increment directory counts */
+	inc_nlink(inode);
+	inc_nlink(d_inode(dentry->d_parent));
+	d_add(dentry, inode);
 fail:
 	return err;
 }
